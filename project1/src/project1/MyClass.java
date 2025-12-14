081479a5be9d4136a1e2a5e7d1d9fd52fb2124a4
akottong-art <akottong@iu.edu>
2025-12-14 15:29:16 -0500

    Add files via upload
    M01 assignment

---
+++ project1/src/project1/MyClass.java
@@ -0,0 +1,9 @@
+package project1;
+
+public class MyClass {
+public static void main (String[] args) {
+	System.out.println("Welcome to Java");
+	System.out.println("Welcome to Computer Science");
+	System.out.println("Programming is fun");
+  }
+}
---
+++ project1/src/project1/project1.java
@@ -0,0 +1,11 @@
+package project1;
+
+public class project1 {
+	public static void main (String[] args) {
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+	}
+}
