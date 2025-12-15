a17fca99935bac7c64fa2eb6e9f4bcdfe30dd07a
akottong-art <akottong@iu.edu>
2025-12-15 02:30:08 -0500

    Add files via upload
    M03 assigments

---
+++ m2/m3/src/m3/Onepoint13.java
@@ -0,0 +1,21 @@
+package m3;
+
+public class Onepoint13 {
+	public static void main (String[] args) {
+		// Coefficients from the equation
+		double a = 3.4;
+		double b = 50.2;
+		double c = 2.1;
+		double d = 0.55;
+		double e = 44.5;
+		double f = 5.9;
+		
+		//Cramer's equation
+		double x = (e * d - b * f) / (a * d - b * c);
+		double y = (a * f - e * c) / (a * d - b * c);
+		
+		//Results
+		System.out.println("x = " + x);
+		System.out.println("y = " + y);
+	}
+}
